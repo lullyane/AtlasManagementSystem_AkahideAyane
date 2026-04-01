289d8acf21bb0c6a0d8c319a2d06665f0cd13953
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-04-01 16:35:50 +0900

    リレーション定義

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -15,7 +15,8 @@ class Subjects extends Model
         'subject'
     ];
 
+    // リレーションの定義
     public function users(){
-        return;// リレーションの定義
+        return $this->belongToMany('App\Models\Users\User','subject_users','subject_id','user_id') ;
     }
 }
--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -66,8 +66,9 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Models\Calendars\ReserveSettings', 'reserve_setting_users', 'user_id', 'reserve_setting_id')->withPivot('id');
     }
 
+    // リレーションの定義
     public function subjects(){
-        return ;// リレーションの定義
+        return $this->belongToMany('App\Models\Users\Subjects','subject_users','user_id','subject_id') ;
     }
 
     // いいねしているかどうか
