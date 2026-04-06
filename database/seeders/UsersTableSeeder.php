3b6a18c951b52e6d17263b5526913f4df034f980
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-04-06 14:48:07 +0900

    初期データの登録

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Users\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        User::create([
+            'over_name' => 'テスト',
+            'under_name' => 'ユーザー',
+            'over_name_kana' => 'テスト',
+            'under_name_kana' => 'ユーザー',
+            'mail_address' => 'test@mail.com',
+            'sex' => 1,
+            'birth_day' => '2000-01-01',
+            'role' => 1,
+            'password' => Hash::make('test'),
+        ]);
     }
 }
