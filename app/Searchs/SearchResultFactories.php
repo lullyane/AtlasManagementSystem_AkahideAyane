b45dc801d4899afa41c26a2ca4d2188079ae7b5d
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-04-14 15:06:08 +0900

    修正：複数の科目で検索した場合、あてはまる科目を一つでも選択しているユーザーはすべて表示させる

--- app/Searchs/SearchResultFactories.php
+++ app/Searchs/SearchResultFactories.php
@@ -5,25 +5,41 @@ use App\Models\Users\User;
 
 class SearchResultFactories{
 
-  // 改修課題：選択科目の検索機能
-  public function initializeUsers($keyword, $category, $updown, $gender, $role, $subjects){
-    if($category == 'name'){
-      if(is_null($subjects)){
-        $searchResults = new SelectNames();
-      }else{
-        $searchResults = new SelectNameDetails();
-      }
-      return $searchResults->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
-    }else if($category == 'id'){
-      if(is_null($subjects)){
-        $searchResults = new SelectIds();
-      }else{
-        $searchResults = new SelectIdDetails();
-      }
-      return $searchResults->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
-    }else{
-      $allUsers = new AllUsers();
-    return $allUsers->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
+    // 改修課題：選択科目の検索機能
+    public function initializeUsers($keyword, $category, $updown, $gender, $role, $subjects){
+        if($category == 'name'){
+        if(is_null($subjects)){
+            $searchResults = new SelectNames();
+        }else{
+            $searchResults = new SelectNameDetails();
+        }
+        return $searchResults->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
+        }else if($category == 'id'){
+        if(is_null($subjects)){
+            $searchResults = new SelectIds();
+        }else{
+            $searchResults = new SelectIdDetails();
+        }
+        return $searchResults->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
+        }else{
+        $allUsers = new AllUsers();
+        return $allUsers->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
+        }
     }
-  }
+
+    public function index(Request $request){
+        $selected = $request->input('subject', []);
+        $users = User::query()
+            ->when($selected, function ($query) use ($selected) {
+                $query->whereHas('subjects', function ($q) use ($selected) {
+                    $q->where(function ($sub) use ($selected) {
+                        foreach ($selected as $id) {
+                            $sub->orWhere('subjects.id', $id);
+                        }
+                    });
+                });
+            })
+            ->get();
+        return view('users.index', compact('users'));
+    }
 }
