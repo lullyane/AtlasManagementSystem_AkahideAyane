3ac644a027ce578fde10f6f29e065de1c8b123f5
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-05-12 14:42:13 +0900

    スクール予約詳細に関するCSSとルート修正

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,21 +1,27 @@
 <x-sidebar>
-<div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
-    <div class="w-50 m-auto h-75">
+<div class="vh-100">
+    <div class="my-5 mx-auto w-75">
         <p><span>{{ \Carbon\Carbon::parse($date)->format('Y年m月d日') }}</span><span class="ml-3">{{ $part }}部</span></p>
-        <div class="h-75 border">
-            <table class="">
-                <tr class="text-center">
-                    <th class="w-25">ID</th>
-                    <th class="w-25">名前</th>
-                    <th class="w-25">場所</th>
-                </tr>
+        <div class="d-flex justify-content-between border border_radius bg-white shadow p-2">
+            <table class="table table-sm w-100 m-0">
+                <thead>
+                    <tr class="text-center reserve_table_header">
+                        <th>ID</th>
+                        <th>名前</th>
+                        <th>場所</th>
+                    </tr>
+                </thead>
+                <tbody class="reserve_table_body">
+                @if($reserve && $reserve->users->isNotEmpty())
                 @foreach($reserve->users as $user)
-                <tr class="text-center">
-                    <td class="w-25">{{ $user->id }}</td>
-                    <td class="w-25">{{ $user->over_name }} {{ $user->under_name }}</td>
-                    <td class="w-25">リモート</td>
-                </tr>
+                    <tr class="text-center">
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->over_name }} {{ $user->under_name }}</td>
+                        <td>リモート</td>
+                    </tr>
                 @endforeach
+                @endif
+                </tbody>
             </table>
         </div>
     </div>
